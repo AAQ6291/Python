49df90ddfed1a9aadb030d119ae2d2f23ad9fdcb
angela_kuo <angelak.tw@gmail.com>
2018-04-18 12:12:50 +0800

    VS Code Git Commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+#from config import DevConfig
+app = Flask(__name__)
+# app.config.from_object(DevConfig)
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/hello/')
+def hello():
+    return 'Hello World!'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
+
+
+def do_the_login():
+    return 'login.....'
+
+
+def show_the_login_form():
+    return 'show login form'
+
+
+if __name__ == '__main__':
+    app.run()
